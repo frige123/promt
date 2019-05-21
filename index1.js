193a0882c2133d3369b2333f454f2263d3b88f77
frige123 <chopkake@gmail.com>
2019-05-21 22:40:15 +0300

    gg

---
+++ index1.js
@@ -0,0 +1,16 @@
+
+let y = prompt('Введите масив');
+function net(y){
+    console.log('y',y)
+    let n;
+    y = +n
+    console.log(n)
+if(n===[]){
+    if(n.lengt >=5){
+       alert('good')
+    }
+    else{
+        alert('bad')
+    }
+}
+}
